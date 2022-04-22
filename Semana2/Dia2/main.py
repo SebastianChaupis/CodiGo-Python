728f3c5f8550f26b5a3bb242e4cd41172834048a
SebastianChaupis <sebaschaupis1@gmail.com>
2022-04-22 01:38:44 -0500

    Dia 2

--- Semana2/Dia2/main.py
+++ Semana2/Dia2/main.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template, request
+import requests
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("tokenfirebase.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+URL ='https://api.github.com/users/SebastianChaupis'
 
 app = Flask(__name__)
 
 
+
+
 @app.route('/')
 def index():
-    nombre = request.args.get('nombre', 'no hay nombre')
     
-    context = {
-        'nombre': nombre
-    }
+    data = requests.get(URL)
+    #nombre = request.args.get('nombre', 'no hay nombre')
+    context = data.json()
+    print(context)
+    
     return render_template('home.html', **context)
 
 @app.route('/peliculas')
@@ -21,4 +34,31 @@ def peliculas():
         'peliculas':listaPeliculas
     }
     return render_template('peliculas.html',**context)
+
+##RUTAS PARA PAGINA
+@app.route('/acercade')
+def about():
+    return render_template('acercade.html')
+
+@app.route('/portafolio')
+def portafolio():
+    colProyectos = db.collection('proyectos')
+    docProyectos = colProyectos.get()
+    #print (docProyectos)
+    lstProyectos=[]
+    for doc in docProyectos:
+        print(doc.to_dict())
+        dicProyecto = doc.to_dict()
+        lstProyectos.append(dicProyecto)
+    
+    context = {
+        'proyectos':lstProyectos
+    }
+    
+    return render_template('portafolio.html',**context)
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
 app.run(debug=True)
